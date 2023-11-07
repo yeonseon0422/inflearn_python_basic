adbbba433e7d975650954f457f314481b8cd566e
sony <142476291+yeonseon0422@users.noreply.github.com>
2023-11-07 17:07:04 +0900

    Add files via upload

---
+++ 1_자료형변수.py
@@ -0,0 +1,22 @@
+# 섹션1-1강
+# 코딩에서 제일 중요한 자료형과 변수 알아보기
+
+# 자료형 = 자료의 형태 = 데이터의 형태
+# 숫자, 문자, 불린, 리스트, 튜플, 딕셔너리
+# 숫자형: 정수 - int(), 실수 - float()
+# 문자형: String
+
+# 변수 = 데이터를 저장할 공간
+# 변수 정의 공식: 변수이름 = 데이터
+name = "마스터이"
+level = 5
+health = 800
+attack = 90
+
+print(name, level, health, attack)
+
+level = level + 1
+health = 900
+attack = 95
+
+print(name, level, health, attack)
