8f287c0f87a1d728a2373e1de21a2a2ab23f7404
sony <142476291+yeonseon0422@users.noreply.github.com>
2023-11-08 15:15:55 +0900

    Add files via upload

---
+++ 2_연산연산자.py
@@ -0,0 +1,47 @@
+# 섹션1-2강
+# 연산과 연산자를 겁나 쉽게 배우기
+
+# 연산: 수나 식을 일정한 규칙에 따라 계산하는 것
+
+# 연산의 종류: 대입연산, 산술연산, 비교연산, 논리연산
+
+# 대입연산: =
+
+name = "강아지"
+print(name)
+
+# 산술연산: 더하기(+), 빼기(-), 곱하기(*), 나누기(/), 몫(//), 나머지(%), 제곱(**)
+
+x = 5
+y = 2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
+text1 = "안녕하세요"
+text2 = "반갑습니다"
+
+total_text = text1 + "\n" + text2 #\n: enter
+print(total_text)
+
+
+# 비교연산: 크다(>), 작다(<), 크거나 같다(>=), 작거나 같다(<=), 같다(==), 다르다(!=)
+# 불린형(Boolean): True, False
+
+print(2 > 3)
+print(15 < 30)
+print(1.5 >= 0)
+print(3 <= 3)
+print(3 == 3)
+print(3 != 3)
+
+
+# 논리연산(불린형끼리의 연산): and, or, not
+
+print(4 < 6 and 10 >= 10)
+print(not 5 == 5)
